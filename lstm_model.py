7d4e7fca98bb824189a520ac1ec1a2de768e26ce
Cynthia Allen <cynthiaallen228@gmail.com>
2024-12-02 21:14:21 -0800

    document

--- lstm_model.py
+++ lstm_model.py
@@ -4,33 +4,41 @@ import torch
 
 import os
 
+# use a gpu if we can
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class FirePredictor(nn.Module):
     def __init__(self,  event_vec_dim, seq_len, hidden_dim, num_layers):
         super(FirePredictor, self).__init__()
+
+        # store some information about the sizes of various things
+        # not all of this is actually used but it doesn't hurt to hold onto it
         self.event_vec_dim = event_vec_dim
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.seq_len = seq_len
 
+        # define some LSTM layers
         self.lstm = nn.LSTM(
             self.event_vec_dim,
             self.hidden_dim,
             num_layers = self.num_layers,
             dropout=0.2,
-            batch_first=True,
+            batch_first=True, # if this is False (the default), the first dimension will be the sequnce, not the batch
             device=device
         )
         self.flatten = nn.Flatten()
         self.output = nn.Linear(hidden_dim*seq_len, event_vec_dim, device=device)
 
     def forward(self, x):
-        h1 = self.lstm(x)[0]
+        h1 = self.lstm(x)[0] # only use the actual outputs, not the "outputs" for the next recurrent layer
         flat = self.flatten(h1)
         return self.output(flat).reshape(x.shape[0], self.event_vec_dim)
 
     def to_json(self, path):
+        """
+        Write the model's state_dict to a JSON file.
+        """
         dir = path.split('/')[0:-1]
         dir = '/'.join(dir)
         os.makedirs(dir, exist_ok=True)
@@ -44,6 +52,9 @@ class FirePredictor(nn.Module):
 
 
     def from_json(self, path):
+        """
+        Set the model's state_dict by reading a JSON file. Everything (namely weight tensors) must be the same size for this to work.
+        """
         # read the JSON
         with open(path) as file:
             text = file.read()
--- preproc.py
+++ preproc.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
 
-# define some contests
-WINDOW_SIZE = 64
-SLIDE_FACTOR = 0.3
-BATCH_SIZE = 64
+# define some constants
+WINDOW_SIZE = 64 # also the sequence length
+SLIDE_FACTOR = 0.3 # how much we slide the window by when making sequences
+BATCH_SIZE = 64 # I don't think this is used by I also don't trust my LSP with jupyter notebooks
 
-# get some min/max values
 def get_bounds(df, cols = None):
+    """
+    Determine the maximum and minimum value for every column listed in `cols`. If `cols` is not specified, do every column.
+    """
     if cols is None:
         cols = df.columns
     bounds = {}
@@ -19,19 +21,30 @@ def get_bounds(df, cols = None):
 
     return bounds
 
-# create a function to scale values into [0, 1]
 def scale(x, min, max):
+    """
+    Scale a value into [0, 1] given the min and max for that measurement.
+    """
     return (x-min) / (max-min)
 
 def unscale(x, min, max):
+    """
+    Undo the scaling for a value given the original min and max for that measurement.
+    """
     rng = max - min
     return (x*rng) + min
 
 def create_map(df, col):
+    """
+    Create a dictionary to map tokens to integers.
+    """
     unique_set = set(df[col].unique())
     return {key: i for i,key in enumerate(unique_set)}
 
 def preprocess(df, bounds):
+    """
+    Preprocess the dataframe *specifically* for our model.
+    """
     res = df.copy()
     # # create the daynight mapping
     # daynight_map = create_map(res, "daynight")
@@ -49,6 +62,9 @@ def preprocess(df, bounds):
     return res
 
 def unprocess(df, bounds):
+    """
+    Undo the effectos of the function `preprocess` for the given data frame.
+    """
     res = df.copy()
     for col in res.columns:
         b = bounds.get(col)
@@ -61,7 +77,10 @@ def unprocess(df, bounds):
     return res
 
 # split into inputs and labels
-def xy_split(df):
+def xy_split(df: pd.DataFrame) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Split into inputs and labels.
+    """
     dset_x = []
     dset_y = []
     for i in range(df.shape[0]-1):
@@ -78,6 +97,9 @@ def xy_split(df):
 # we need to do this because we cannot randomly pick readings from the dataset and still have valid inputs
 # | their relative location is *very* important
 def sequencify(xs, ys):
+    """
+    Arrange the given x and y values into sequences using a sliding window algorithm.
+    """
     sequence_x = []
     sequence_y = []
     for i in range(0, xs.shape[0]-WINDOW_SIZE, int(WINDOW_SIZE*SLIDE_FACTOR)):
